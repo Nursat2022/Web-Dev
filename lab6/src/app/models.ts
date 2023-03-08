c36d8927ed6424023d8326200a49b1b914a43fd4
Nursat2022 <98874935+Nursat2022@users.noreply.github.com>
2023-03-09 00:08:54 +0600

    Add files via upload

---
+++ lab6/src/app/models.ts
@@ -0,0 +1,17 @@
+export interface Album {
+  id: number;
+  title: string;
+
+}
+
+
+export interface Photo {
+
+  album_id: number,
+  id: number;
+  title: string;
+  url: string;
+  thumbnailUrl: string;
+}
+
+
